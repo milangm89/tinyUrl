498f0652705c7037d5dd6414249419790a6dfc70
Surya Singh <me@suryasingh.xyz>
2017-08-26 05:00:16 +0530

    emtry string done

--- controllers/index.js
+++ controllers/index.js
@@ -1,51 +1,102 @@
 'use strict';
-
+var randomstring = require("randomstring");
 var UrlData = require('../models/db');
 
 module.exports = {
-  index: {
-    get(req, res) {
-      res.render('index',{
-      	title: "TinyUrl"
-      });
+    index: {
+        get(req, res) {
+            res.render('index', {
+                title: "TinyUrl"
+            });
+        },
+
+        post(req, res, next) {
+            var regexp1 = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+            var urlmatch1 = req.body.longurl.match(regexp1);
+            var regexp2 = /^(?:[a-z0-9A-Z]{0}|[A-Za-z0-9]{7})$/;
+            var urlmatch2 = req.body.shorturl.match(regexp2);
+            console.log("1->" + urlmatch1);
+            console.log("2->" + urlmatch2);
+
+            var custom = false;
+
+            if (req.body.shorturl !== "") {
+                custom = true;
+            }
+
+            if (urlmatch1 !== null && urlmatch2 !== null) {
+
+                if (custom === true) {
+                    var items = {
+                        longurl: req.body.longurl,
+                        shorturl: req.body.shorturl
+                    };
+                } else {
+                    var surl = randomstring.generate({
+                        length: 7,
+                        charset: 'alphanumeric'
+                    });
+                    var items = {
+                        longurl: req.body.longurl,
+                        shorturl: surl
+                    };
+                }
+
+
+                // } 
+                // custom === true) {
+                var data = new UrlData(items);
+
+                data.save(function(err, doc) {
+                    if (err) {
+                        res.json(err);
+                    } else {
+                        console.log(doc);
+                        res.render("new-url", {
+                            title: "TinyUrl",
+                            longurl: req.body.longurl,
+                            shorturl: req.body.shorturl
+                        });
+                    }
+                });
+                // } else if (urlmatch1 !==null && 
+                //          urlmatch2 !== null && 
+                //          custom === false) {
+
+                // var surl = randomstring.generate({
+                //     length: 7,
+                //     charset: 'alphanumeric'
+                // });
+
+                // console.log("Hash -- > " + surl);
+                // var data = new UrlData({
+                //     longurl: req.body.longurl,
+                //     // create new short url
+                //     shorturl: surl
+                // });
+
+                // data.save(function(err, doc) {
+                //     if (err) {
+                //         res.json(err);
+                //     } else {
+                //         console.log(doc);
+                //         res.render("new-url", {
+                //             title: "TinyUrl",
+                //             longurl: req.body.longurl,
+                //             shorturl: surl
+                //         });
+                //     }
+                // });
+
+            } else {
+                if (urlmatch1 === null) {
+                    res.send("Enter complete long url e.g. https://www.regex101.com/")
+                } else if (urlmatch2 === null) {
+                    res.send("Short URL must be 7 characters long and it must carry only A-Za-z0-9");
+                }
+
+            }
+
+        },
     },
-
-    post(req,res,next) {
-    	var regexp1 = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-    	var urlmatch1 = req.body.longurl.match(regexp1);
-
-    	var regexp2 = /^[A-Za-z0-9_]{7}$/;
-    	var urlmatch2 = req.body.shorturl.match(regexp2);
-
-    	if (urlmatch1 !=null && urlmatch2 !=null) {
-    		var data = new UrlData({
-    		longurl: req.body.longurl,
-    		shorturl: req.body.shorturl
-    		});
-
-    	data.save(function(err, doc){
-		if(err){
-			res.json(err);
-		} 
-		else {
-			console.log(doc);
-			res
-			res.render("new-url",{
-				title: "TinyUrl",
-				longurl: req.body.longurl,
-				shorturl: req.body.shorturl
-			});
-		}   
-    });
-    	} else {
-    		if (urlmatch1 == null) {
-    			res.send("Enter complete long url e.g. https://www.regex101.com/")
-    		} else {
-    			res.send("Short URL must be 7 characters long and it must carry only A-Za-z0-9_");
-    		}
-    		
-    	}
-    	
-  },
-  },
 };

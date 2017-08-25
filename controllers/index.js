8241ede5a60414365cb9dd83047724355e06ee63
Surya Singh <me@suryasingh.xyz>
2017-08-26 00:17:31 +0530

    regex match for longurl

--- controllers/index.js
+++ controllers/index.js
@@ -11,10 +11,14 @@ module.exports = {
     },
 
     post(req,res,next) {
-    	var data = new UrlData({
+    	var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    	var urlmatch = req.body.longurl.match(regexp);
+
+    	if (urlmatch !=null) {
+    		var data = new UrlData({
     		longurl: req.body.longurl,
     		shorturl: req.body.shorturl
-    	});
+    		});
 
     	data.save(function(err, doc){
 		if(err){
@@ -25,6 +29,10 @@ module.exports = {
 			res.json(doc);
 		}   
     });
+    	} else {
+    		res.send("Enter complete url e.g. https://www.regex101.com/")
+    	}
+    	
   },
   },
 };

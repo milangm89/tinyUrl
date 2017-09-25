d101590b7c12565be888c0d77168dbbab129d2bc
Surya Singh <me@suryasingh.xyz>
2017-09-25 14:33:52 +0530

    rv

--- controllers/index.js
+++ controllers/index.js
@@ -1,82 +1,82 @@
 'use strict';
-var randomstring = require('randomstring');
-var UrlData = require('../models/db');
+const randomstring = require('randomstring');
+const UrlData = require('../models/db');
 
-module.exports = {
-    index: {
-        get(req, res) {
-            res.render('index', {
-                title: 'TinyUrl'
-            });
-        },
 
-        post(req, res, next) {
-            // regex to check if input is a url for long url of the format https://facebook.com
-            var regexp1 = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-            var urlmatch1 = req.body.longurl.match(regexp1);
-            // regex to chekc if input contains only alphanumeric characters for custom short url
-            var regexp2 = /^(?:[a-z0-9A-Z]{0}|[A-Za-z0-9]{7})$/;
-            var urlmatch2 = req.body.shorturl.match(regexp2);
-            var custom = false;
+// get request to handle title
+exports.index_get = function(req, res) {
+    res.render('index', {
+        title: 'TinyUrl'
+    });
+};
 
-            if (req.body.shorturl !== '') {
-                custom = true;
-            }
+// post request to handle url shortening
+exports.index_post = function(req, res, next) {
 
-            if (urlmatch1 !== null && urlmatch2 !== null) {
+    // regex to check if input is a url for long url of the format https://facebook.com
+    let regexp1 = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    let urlmatch1 = req.body.longurl.match(regexp1);
 
-                var inpx = req.body.longurl,
-                    inpy = req.body.shorturl;
+    // regex to chekc if input contains only alphanumeric characters for custom short url
+    let regexp2 = /^(?:[a-z0-9A-Z]{0}|[A-Za-z0-9]{7})$/;
+    let urlmatch2 = req.body.shorturl.match(regexp2);
+    let custom = false;
 
-                //  query to check if short url exits
-                var query = UrlData.findOne({ 'longurl': req.body.longurl },
-                    function(err, doc) {
-                        if (err) {
-                            res.send(err)
-                        }
+    if (req.body.shorturl !== '') {
+        custom = true;
+    }
 
-                        if (doc === null) {
+    if (urlmatch1 !== null && urlmatch2 !== null) {
 
-                            // create short links
-                            if (custom === true) {
-                                var surl = req.body.shorturl;
-                            } else {
-                                var surl = randomstring.generate({
-                                    length: 7,
-                                    charset: 'alphanumeric'
-                                });
-                            }
+        let inpx = req.body.longurl,
+            inpy = req.body.shorturl;
 
-                            var items = {
-                                longurl: req.body.longurl,
-                                shorturl: surl
-                            }
-                            var data = new UrlData(items);
-                            inpy = items.shorturl;
-                            data.save();
-                        } else {
-                            inpy = doc.shorturl;
-
-                        }
-
-                        // res.redirect('http://google.com');
-
-                        res.render('new-url', {
-                            title: 'TinyUrl',
-                            longurl: req.body.longurl,
-                            shorturl: inpy
-                        });
-                    });
-
-            } else {
-                if (urlmatch1 === null) {
-                    res.send('Enter complete long url e.g. https://www.regex101.com/')
-                } else if (urlmatch2 === null) {
-                    res.send('Short URL must be 7 characters long and it must carry only A-Za-z0-9');
+        //  query to check if short url exits
+        let query = UrlData.findOne({ 'longurl': req.body.longurl },
+            function(err, doc) {
+                if (err) {
+                    res.send(err)
                 }
 
-            }
+                if (doc === null) {
 
-        },
-    },
+                    // create short links
+                    if (custom === true) {
+                        var surl = req.body.shorturl;
+                    } else {
+                        var surl = randomstring.generate({
+                            length: 7,
+                            charset: 'alphanumeric'
+                        });
+                    }
+
+                    let items = {
+                        longurl: req.body.longurl,
+                        shorturl: surl
+                    }
+                    let data = new UrlData(items);
+                    inpy = items.shorturl;
+                    data.save();
+                } else {
+                    inpy = doc.shorturl;
+
+                }
+
+                // res.redirect('http://google.com');
+
+                res.render('new-url', {
+                    title: 'TinyUrl',
+                    longurl: req.body.longurl,
+                    shorturl: inpy
+                });
+            });
+
+    } else {
+        if (urlmatch1 === null) {
+            res.send('Enter complete long url e.g. https://www.regex101.com/')
+        } else if (urlmatch2 === null) {
+            res.send('Short URL must be 7 characters long and it must carry only A-Za-z0-9');
+        }
+
+    }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
-let express    = require('express');
+let express = require('express');
 let Index = require('../controllers/index');
 let TinyUrl = require('../controllers/tinyurl');
-let router     = express.Router();
+let router = express.Router();
 
-router.get('/', Index.index.get);
-router.post('/new-url', Index.index.post);
-router.get('/:url?', TinyUrl.index.get);
+
+
+router.get('/', Index.index_get);
+router.post('/new-url', Index.index_post);
+router.get('/:url?', TinyUrl.index_get);
 
 module.exports = router;

8a636b6cd362ac1807d6dd967f4e2359948c846d
Surya Singh <me@suryasingh.xyz>
2017-09-25 12:55:25 +0530

    rv13

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,25 @@
 'use strict';
-var randomstring = require("randomstring");
+var randomstring = require('randomstring');
 var UrlData = require('../models/db');
 
 module.exports = {
     index: {
         get(req, res) {
             res.render('index', {
-                title: "TinyUrl"
+                title: 'TinyUrl'
             });
         },
 
         post(req, res, next) {
+            // regex to check if input is a url for long url of the format https://facebook.com
             var regexp1 = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
             var urlmatch1 = req.body.longurl.match(regexp1);
+            // regex to chekc if input contains only alphanumeric characters for custom short url
             var regexp2 = /^(?:[a-z0-9A-Z]{0}|[A-Za-z0-9]{7})$/;
             var urlmatch2 = req.body.shorturl.match(regexp2);
             var custom = false;
 
-            if (req.body.shorturl !== "") {
+            if (req.body.shorturl !== '') {
                 custom = true;
             }
 
@@ -59,8 +61,8 @@ module.exports = {
 
                         // res.redirect('http://google.com');
 
-                        res.render("new-url", {
-                            title: "TinyUrl",
+                        res.render('new-url', {
+                            title: 'TinyUrl',
                             longurl: req.body.longurl,
                             shorturl: inpy
                         });
@@ -68,9 +70,9 @@ module.exports = {
 
             } else {
                 if (urlmatch1 === null) {
-                    res.send("Enter complete long url e.g. https://www.regex101.com/")
+                    res.send('Enter complete long url e.g. https://www.regex101.com/')
                 } else if (urlmatch2 === null) {
-                    res.send("Short URL must be 7 characters long and it must carry only A-Za-z0-9");
+                    res.send('Short URL must be 7 characters long and it must carry only A-Za-z0-9');
                 }
 
             }
--- models/db.js
+++ models/db.js
@@ -1,29 +1,30 @@
 var mongoose = require('mongoose');
+var config = require('../config/config.js').get(process.env.NODE_ENV);
+
 // var autoIncrement = require('mongoose-auto-increment'); 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/tinyurl');
+mongoose.connect(config.database);
 // autoIncrement.initialize(mongoose.connect);
 
 var Schema = mongoose.Schema;
 mongoose.set('debug', true);
 
 var urlSchema = new Schema({
-	// _id : {
-	// 	type: Number,
-	// 	required: true
-	// },
-	longurl : {
-		type: String,
-		unique: true,
-		required: true
-	},
-	shorturl : {
-		type: String,
-		unique: true,
-		required: true
-	}
-}, {collection : 'tinyurl'},
-{ versionKey: false });
+    // _id : {
+    // 	type: Number,
+    // 	required: true
+    // },
+    longurl: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    shorturl: {
+        type: String,
+        unique: true,
+        required: true
+    }
+}, { collection: 'tinyurl' }, { versionKey: false });
 
 var UrlData = mongoose.model('UrlData', urlSchema)
 
